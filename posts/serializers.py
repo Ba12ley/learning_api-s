f67c301fbfc3e90900e56d03f654736db4312fdf
Ba12ley <baiz@me.com>
2021-08-17 03:08:40 +0100

    playing with permissions and mixins

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,13 +1,28 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models import Post
 
-# create serialixer
+
+User = get_user_model()
+
+class OwnerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'username'
+        )
+
+# create serializer
 class PostSerializer(serializers.ModelSerializer):
+
+    owner = serializers.HyperlinkedIdentityField(many=False, view_name='owner-detail')
     # required meta
     class Meta:
         model = Post
         fields = (
             'pk',
+            'owner',
             'title',
             'custom_id',
             'category',
